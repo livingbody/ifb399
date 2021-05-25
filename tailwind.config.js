ef0f352a1ba006568f9ac1198ec49062197563b0
ralph0813 <ralph0813.ljt@gmail.com>
2021-05-26 00:39:27 +0800

    remove theme:outline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,19 @@
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: {
-                brand: {
-                    light: '#76b3e8',
-                    DEFAULT: '#64a6d4',
-                    dark: '#4c91ba'
-                }
-            },
-            outline: {
-                blue: '2px solid #0000ff',
-            }
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: '#76b3e8',
+          DEFAULT: '#64a6d4',
+          dark: '#4c91ba',
         },
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [
-        require('@tailwindcss/forms')
-    ]
-};
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
