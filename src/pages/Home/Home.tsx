638143aec8e686c0a4c75208d71263383018a9a5
ralph <ralph0813.ljt@gmail.com>
2021-08-16 22:47:14 +0800

    Add usefetch

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import HeroSection from '../../components/HeroSection/HeroSection'
+import useFetch from '../../hooks/useFetch'
 
 const Home = () => {
+  const { data, loading, error } = useFetch('https://api.quotable.io/random')
+  console.log({ data, loading, error })
   return <HeroSection />
 }
 
