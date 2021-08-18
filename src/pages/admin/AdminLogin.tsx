ebed003f091a4ee5a066be65705c061b8936d694
Juntao <ralph0813.ljt@gmail.com>
2021-08-18 10:07:45 +0800

    Fix ios height 100vh issue.

--- src/pages/admin/AdminLogin.tsx
+++ src/pages/admin/AdminLogin.tsx
@@ -6,7 +6,7 @@ import bg from '../../static/bg_admin_login.jpg'
 export default function Login() {
   let history = useHistory()
   return (
-    <div className='min-h-screen bg-white flex'>
+    <div className='min-h-screen min-h-screen-ios bg-white flex'>
       <div className='flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24'>
         <div className='mx-auto w-full max-w-sm lg:w-96'>
           <div>
